4c4a03b5ec6f52df2c3810dfa4b91ce796a92b50
saintkim12 <saintkim1232@gmail.com>
2022-06-17 14:23:43 +0900

    화면 작업 및 로직 작업 중

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  plugins: ['html'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  globals: {
+    PetiteVue: true
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-unused-vars': 'warn'
+  },
+}
